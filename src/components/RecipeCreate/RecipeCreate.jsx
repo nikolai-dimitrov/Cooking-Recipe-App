1d95f2d5573fcfd7fe79d580cd42aa2a526d2552
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2023-11-17 17:27:22 +0200

    Added recipe create page

--- src/components/RecipeCreate/RecipeCreate.jsx
+++ src/components/RecipeCreate/RecipeCreate.jsx
@@ -1,3 +1,15 @@
+import styles from "./recipe-create.module.css";
+import { RecipeForm } from "../RecipeForm/RecipeForm";
 export const RecipeCreate = () => {
-    return;
+    return (
+        <section className={styles.create}>
+            <div className={styles.container}>
+                <h1 className={styles.form__title}>Create</h1>
+                <p className={styles.form__subtitle}>
+                    Share your recipes with us
+                </p>
+                <RecipeForm btnName={'Create'}/>
+            </div>
+        </section>
+    );
 };
