c71ad514db0e907b1d0d044aaf710d47b3463924
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2023-11-22 18:43:47 +0200

    Added logout component and functionality.

--- src/components/Logout/Logout.jsx
+++ src/components/Logout/Logout.jsx
@@ -1,3 +1,11 @@
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext";
 export const Logout = () => {
-    return;
+    const { logoutHandler } = useContext(AuthContext);
+    const navigate = useNavigate();
+    useEffect(() => {
+        logoutHandler();
+        navigate("/login");
+    }, []);
 };
