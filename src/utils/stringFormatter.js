1ec9be8e1ab1e9230f2c89ea8f75a375ddc1f02c
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2023-11-22 18:41:05 +0200

    Added string format utils.

---
+++ src/utils/stringFormatter.js
@@ -0,0 +1,21 @@
+export const fromStringToObject = (data) => {
+    let ingredients = data.ingredients
+        .split(";")
+        .filter((el) => el.trim() != "");
+    let tuplesIngredients = ingredients.map((i) => i.trim().split("-"));
+    let formattedIngredients = tuplesIngredients.map(([k, v]) => [
+        k.trim(),
+        v.trim(),
+    ]);
+    let ingredientsObj = Object.fromEntries(formattedIngredients);
+    return ingredientsObj;
+};
+
+export const fromObjectToString = (data) => {
+    let tuplesIngredients = Object.entries(data);
+    let arrayIngredients = tuplesIngredients
+        .map((arr) => arr.join(" - "))
+        .join("; ");
+    // console.log(asd.join('; '))
+    return arrayIngredients;
+};
