158c4fd6cc4a77977dfb67aaae29a371eebc2e24
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2023-12-05 14:59:42 +0200

    Added profile picture.

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,21 +1,40 @@
-import { createContext } from "react";
+import { createContext, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useLocalStorage } from "../hooks/useLocalStorage";
 import { authServiceFactory } from "../services/authService";
 export const AuthContext = createContext();
+const defaultPicture =
+    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUphuF-TaDSPNi8FK1FdXKTjKmUk3RUln8zyFtsnfbRw&s";
 
+//User's profile additional info like first name, last name must be in another collection like Profile.
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useLocalStorage("user", {});
+    const [profileImg, setProfileImg] = useState(defaultPicture);
     const navigate = useNavigate();
     const authService = authServiceFactory();
     //TODO: Validation and error handling
+    useEffect(() => {
+        if (user._id) {
+            authService.getProfileImg(user._id).then((p) => {
+                console.log(user._id, "USER ID FETCHING");
+                if (p.length > 0) {
+                    const image = p[0].pictureUrl;
+                    setProfileImg((state) => image);
+                } else {
+                    setProfileImg(defaultPicture);
+                }
+            });
+        }
+    }, [user, user._id]); // remove user._id
+
     const loginSubmitHandler = async (formValues) => {
         let result = await authService.login(formValues);
-        const { accessToken, email, _id } = { ...result };
-
+        const { accessToken, email, _id, firstName, lastName } = { ...result };
         setUser({
             accessToken,
             email,
+            firstName,
+            lastName,
             _id,
         });
         navigate("/");
@@ -24,10 +43,12 @@ export const AuthProvider = ({ children }) => {
     //TODO: Validation and error handling
     const registerSubmitHandler = async (formValues) => {
         let result = await authService.register(formValues);
-        const { accessToken, email, _id } = { ...result };
+        const { accessToken, email, _id, firstName, lastName } = { ...result };
         setUser({
             accessToken,
             email,
+            firstName,
+            lastName,
             _id,
         });
         navigate("/");
@@ -37,18 +58,22 @@ export const AuthProvider = ({ children }) => {
         authService.logout();
         setUser({});
     };
-
+    const changeProfileImgHandler = (formValues) => {
+        console.log(formValues, "AUTH CONTEXT HANDLER");
+    };
     const authContext = {
         loginSubmitHandler,
         registerSubmitHandler,
         logoutHandler,
+        changeProfileImgHandler,
         token: user.accessToken,
         email: user.email,
         _id: user.id,
         isAuthenticated: !!user.accessToken,
         user,
+        profileImg,
     };
-
+    console.log(profileImg, "final log");
     return (
         <AuthContext.Provider value={authContext}>
             {children}
