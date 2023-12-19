379611c99c741b708545050eb4da41c9c7c49bc1
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2023-12-19 19:26:03 +0200

    Deleted file

--- client/src/assets/docs.js
+++ client/src/assets/docs.js
@@ -1,8 +1,1 @@
 
-
-// Tuesday:
-// 2.Good documentation for this project with screenshots and all... !
-// 1.Profile and nav media queries
-// 3.To start think and do something about MERN - Stack APP ->( it can we done sunday instead of typescript ) ?
-
-// Next week to watch 2 lectures to finish ReactJS course
