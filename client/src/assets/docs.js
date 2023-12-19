c8f517984a4ea03561e9524f207612f1d627ce68
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2023-12-19 19:15:45 +0200

    Changed file structure

--- client/src/assets/docs.js
+++ client/src/assets/docs.js
@@ -1,10 +1,8 @@
-// Monday
+
 
 // Tuesday:
 // 2.Good documentation for this project with screenshots and all... !
+// 1.Profile and nav media queries
 // 3.To start think and do something about MERN - Stack APP ->( it can we done sunday instead of typescript ) ?
-
-// Wednesday:
-// 2.To check typescript. ?
 
 // Next week to watch 2 lectures to finish ReactJS course
