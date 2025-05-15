85a5677bfa17b3d20d48cd9fcaa5b0fab99f1da0
nikolai-dimitrov <120419122+nikolai-dimitrov@users.noreply.github.com>
2025-05-15 17:38:12 +0300

    feat: add package json and change server port

--- server/server.js
+++ server/server.js
@@ -1579,11 +1579,13 @@
         requestHandler(plugins, services)
     );
 
-    const port = 3030;
-    server.listen(port);
-    console.log(
-        `Server started on port ${port}. You can make requests to http://localhost:${port}/`
-    );
+    const port = process.env.PORT || 3030;
+    server.listen(port, '0.0.0.0', () => {
+        console.log(
+            `Server started on port ${port}. You can make requests to http://localhost:${port}/`
+        );
+    });
+    
     console.log(`Admin panel located at http://localhost:${port}/admin`);
 
     var softuniPracticeServer = {};
